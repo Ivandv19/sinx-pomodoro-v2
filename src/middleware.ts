3b0e035a655af1fea23cd21b56db108ca900f1c7
Ivan <ivangtx19@gmail.com>
2026-02-02 13:01:24 -0600

    fix: remove mardown syntax from middleware code

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,6 @@
-```typescript
 import { defineMiddleware } from "astro:middleware";
 import { initializeLucia } from "./lib/auth";
 
-export const onRequest = defineMiddleware(async (context, next) => {
-    // 5. Set locals
-    // context.locals.session = session;
-    // context.locals.user = user;
-
+export const onRequest = defineMiddleware((context, next) => {
     return next();
 });
-
-```
